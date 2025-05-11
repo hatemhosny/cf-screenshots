7ffda332e6a85f3e65d6d67228c841b0617d766e
Hatem Hosny <hatemhosny@gmail.com>
2025-05-11 22:47:23 +0300

    cors headers

--- functions/api/screenshot.ts
+++ functions/api/screenshot.ts
@@ -49,8 +49,7 @@ export const onRequestOptions: PagesFunction = async () => {
     headers: {
       "Access-Control-Allow-Origin": "*",
       "Access-Control-Allow-Headers": "*",
-      "Access-Control-Allow-Methods": "POST, OPTIONS",
-      "Access-Control-Max-Age": "86400",
+      "Access-Control-Allow-Methods": "*",
     },
   });
 };
